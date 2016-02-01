dd9ea722fa44146e4d8e20f5d1a38a14ea9203b9
zemerickj <zemerickj@leidos.com>
2016-02-01 13:41:36 -0500

    Wiring up example UI test.

--- src/test/java/org/jboss/as/quickstarts/kitchensink/test/ExampleUserInterfaceIT.java
+++ src/test/java/org/jboss/as/quickstarts/kitchensink/test/ExampleUserInterfaceIT.java
@@ -1,34 +1,46 @@
 package org.jboss.as.quickstarts.kitchensink.test;
 
-import java.io.IOException;
+import static org.junit.Assert.*;
 
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class ExampleUserInterfaceIT {
 
-	/*@Before
+	private WebDriver driver;
+	
+	@Before
 	public void before() {
 		
 		System.setProperty("webdriver.chrome.driver", "/usr/bin/chromium-browser");
 		
-	}*/
+	}
+	
+	@After
+	public void after() {
+		
+		driver.quit();
+		
+	}
+	
 	
 	@Test
 	public void titleTest() throws IOException {
 		
-		WebDriver driver = new FirefoxDriver();
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		driver.navigate().to("http://localhost:8080/");
+		driver.get("http://localhost:8080/pages/index.html");
+		String text = driver.findElement(By.ById.id("pageTitle")).getText();
 		
-		String appTitle = driver.getTitle();
-		
-		System.out.println("Application title is: " + appTitle);
-		
-		driver.quit();
+		assertEquals("My Dashboard", text);
 		
 	}
 
